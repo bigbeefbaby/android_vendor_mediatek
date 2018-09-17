05329ce1b129631964785c5c1b55670cab807aa3
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-09-17 18:42:16 +0200

    {'modified': ['config/board.mk', 'config/common.mk']} lineage-14.1

--- config/common.mk
+++ config/common.mk
@@ -6,98 +6,19 @@ PRODUCT_PACKAGES += \
     libmtkshim_log \
     libmtkshim_audio \
     libmtkshim_ui \
-    libmtkshim_gui \
     libmtkshim_omx \
-    libmtkshim_camera \
     libmtkshim_gps
 
 # Display
 PRODUCT_PACKAGES += \
     libion
 
-# Gralloc
-PRODUCT_PACKAGES += \
-    android.hardware.graphics.allocator@2.0-impl \
-    android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.renderscript@1.0-impl
-
-# HW Composer
-PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.composer@2.1-service
-
-# Light HAL
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service
-
-# Bluetooth
-PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service
-
-# Camera
-PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
-
-# Sensors
-PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
-
-# Keymaster
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
-
-# Memtrack
-PRODUCT_PACKAGES += \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service
-
-# VibratorHAL
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
-
-# UsbHAL
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
-
 # GPS
-PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
-    android.hardware.gnss@1.0-service \
-    libcurl
-
 PRODUCT_COPY_FILES += \
     vendor/mad/prebuilt/etc/agps_profiles_conf2.xml:system/etc/agps_profiles_conf2.xml
 
-# Jelly
 PRODUCT_PACKAGES += \
-    Jelly
-
-# OmniStyle
-PRODUCT_PACKAGES += \
-    OmniStyle
-
-# OmniJaws
-PRODUCT_PACKAGES += \
-    OmniJaws
-
-# madCamera
-PRODUCT_PACKAGES += \
-    madCamera
-
-# madWallpapers
-PRODUCT_PACKAGES += \
-    madWallpapers
-
-# madLauncher
-PRODUCT_PACKAGES += \
-    madLauncher
+    libcurl
 
 # Include explicitly to work around Facelock issues
 PRODUCT_PACKAGES += \
@@ -117,6 +38,7 @@ PRODUCT_PACKAGES += \
 
 # exFAT
 PRODUCT_PACKAGES += \
+    mount.exfat \
     fsck.exfat \
     mkfs.exfat
 
@@ -130,11 +52,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     com.android.future.usb.accessory
 
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images \
-    mad_charger_res_images
-
 # WallpaperPicker
 PRODUCT_PACKAGES += \
     WallpaperPicker
@@ -142,10 +59,6 @@ PRODUCT_PACKAGES += \
 # Sensor Calibration
 PRODUCT_PACKAGES += \
     libem_sensor_jni
-
-# Audio Settings
-PRODUCT_PACKAGES += \
-    libaudio_volume_jni
 
 # ThemeInterfacer
 PRODUCT_PACKAGES += \
@@ -155,46 +68,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Eleven
 
-# Bootanimation
-ifneq ($(filter 720,$(TARGET_SCREEN_WIDTH)),)
-    PRODUCT_COPY_FILES += \
-        vendor/mad/prebuilt/bootanimation/720p.zip:system/media/bootanimation.zip
-endif
-ifneq ($(filter 1080,$(TARGET_SCREEN_WIDTH)),)
-    PRODUCT_COPY_FILES += \
-        vendor/mad/prebuilt/bootanimation/1080p.zip:system/media/bootanimation.zip
-endif
-
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-
-# WiFi Display
-# this property enables the user to access Google WFD settings.
+# Granular Volume Steps
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.debug.wfd.enable=1
+    ro.config.vc_call_vol_steps=14 \
+    ro.config.media_vol_steps=30
 
-# SELinux
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.selinux=1
+    ro.build.selinux=0
 
 # madOS Versioning
-ANDROID_VERSION = 8.1.0
-MADOS_VERSION = 2.1.3
-
-ifndef MADOS_BUILD_TYPE
-    MADOS_BUILD_TYPE := OFFICIAL
-endif
-
-MADOS_MOD_VERSION := madOS-$(MADOS_VERSION)-$(shell date -u +%Y%m%d)-$(MADOS_BUILD_TYPE)
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.mados.version=$(MADOS_VERSION) \
-    ro.mados.releasetype=$(MADOS_BUILD_TYPE) \
-    ro.modversion=$(MADOS_MOD_VERSION)
-
-MADOS_DISPLAY_VERSION := madOS-$(MADOS_VERSION)-$(MADOS_BUILD_TYPE)
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.mados.display.version=$(MADOS_DISPLAY_VERSION)
+ANDROID_VERSION = 7.1.2
 
 # include other configs
 include vendor/mad/config/permissions.mk
@@ -202,6 +85,3 @@ include vendor/mad/config/media.mk
 include vendor/mad/config/wifi.mk
 include vendor/mad/config/telephony.mk
 include vendor/mad/config/google_override.mk
-
-# DEBUG_BOOT
-include vendor/mad/config/debug_boot.mk
